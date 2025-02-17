dd3feba6cdbf0d2bb59d570e285d76266fdcf8bd
Nash504 <nashdfern@gmail.com>
2025-02-17 19:16:41 +0300

    skeleton

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -62,6 +62,16 @@ export default function Home() {
               placeholder="Paste your notes here..."
               required
             />
+            <CardDescription>
+              {input.length > 25000 ? (
+                <p className="text-red-500">
+                  {" "}
+                  You have exceeded the maximum of 25,000 characters
+                </p>
+              ) : (
+                ""
+              )}
+            </CardDescription>
             <CardDescription className=" text-right mt-2 mr-auto text-lg">
               {input.length < 25000 ? (
                 <p>{input.length}/25000 characters</p>
@@ -77,15 +87,18 @@ export default function Home() {
           </CardFooter>
         </Card>
       </form>
-      <Card className="border-black mt-4 p-4 max-w-xl mx-auto border-4">
-        {loading ? (
-          <div className="flex justify-center items-center mx-auto pt-8">
-            <Skeleton className="h-[200px] w-full max-w-xl rounded-xl" />
-          </div>
-        ) : (
-          output && <p>{output}</p>
-        )}
-      </Card>
+
+      {loading ? (
+        <div className="flex justify-center items-center mx-auto pt-8">
+          <Skeleton className="h-[200px] w-full max-w-xl rounded-xl" />
+        </div>
+      ) : (
+        output && (
+          <Card className="border-black mt-4 p-4 max-w-xl mx-auto border-4">
+            <p>{output}</p>{" "}
+          </Card>
+        )
+      )}
     </div>
   );
 }
