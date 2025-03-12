3209feacf7b268284f08ab7b6bdaebc59fc1b6b0
Nash504 <nashdfern@gmail.com>
2025-03-12 21:41:54 +0530

    about page

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -16,18 +16,29 @@ export default function Home() {
           <h1 className="text-3xl font-bold text-left p-2 text-[#58CC02] ">
             About Us
           </h1>
-          <p className="text-lg text-left"> 
-            Welcome to ByteSize – "Simplifying Revision, One Byte at a Time."<br /><br/>
-            We are a team of students from St. Aloysius Degree College, bringing you ByteSize,<br/><br/> An AI-powered flashcard generator that helps students quickly turn topics into smart, bite-sized flashcards for easy learning and revision.<br/><br/>
-            Built with Next.js, ByteSize is designed for efficiency, ensuring a smooth, user-friendly experience. Stay organized, stay ahead—ByteSize makes studying smarter, not harder. 
+          <p className="text-lg text-left">
+            Welcome to ByteSize – "Simplifying Revision, One Byte at a Time."
+            <br />
+            <br />
+            We are a team of sexy students from St. Aloysius Degree College,
+            bringing you ByteSize,
+            <br />
+            <br /> An AI-powered flashcard generator that helps students quickly
+            turn topics into smart, bite-sized flashcards for easy learning and
+            revision.
+            <br />
+            <br />
+            Built with Next.js, ByteSize is designed for efficiency, ensuring a
+            smooth, user-friendly experience. Stay organized, stay
+            ahead—ByteSize makes studying smarter, not harder.
           </p>
         </div>
 
-        <div className="flex flex-col gap-4 mb-4 mt-4"> 
+        <div className="flex flex-col gap-4 mb-4 mt-4">
           <h1 className="text-3xl font-bold text-left text-[#58CC02]">
             Meet the Team
           </h1>
-          <br/>
+          <br />
           <div className="flex flex-row flex-wrap gap-4">
             <Profile
               img={srijan}
@@ -35,9 +46,8 @@ export default function Home() {
               linkedin_link="https://www.linkedin.com/in/srijan-kulal"
               github_link="https://github.com/srijankulal"
               Description={srijandes}
-
             />
-        
+
             <Profile
               img={nash}
               name="Nash Fernandes"
